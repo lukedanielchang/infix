f6c344f5b5c9e8e569065407836c92f4e3ee1027
luke <luke@gmail.com>
2018-08-26 18:23:11 -0400

    adding evaluation class 	tokenize

--- src/infix/Evaluation.java
+++ src/infix/Evaluation.java
@@ -5,10 +5,29 @@
  */
 package infix;
 
+import java.util.Stack;
+import java.util.StringTokenizer;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author ldcha
  */
 public class Evaluation {
+
+    public boolean isEmpty = false;
     
+    public String newResult = "this will be the result";
+    public Stack operatorS = new Stack();
+    public Stack operandS = new Stack();
+
+    
+     public String evaluate(String fromInput) {
+            StringTokenizer tokenString = new StringTokenizer(fromInput);
+            
+            for (int i =1; tokenString.hasMoreTokens(); i++){
+                System.out.println("Token " + i +":" + tokenString.nextToken());
+            }
+                return newResult;
+    }
 }
