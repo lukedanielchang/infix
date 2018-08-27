edafcc4b4d2d8e221203b02cb71185693097ddd1
luke <luke@gmail.com>
2018-08-26 20:48:28 -0400

    commit 8:11 PM looping through initial tokenized array[] tokens

--- src/infix/Evaluation.java
+++ src/infix/Evaluation.java
@@ -17,29 +17,44 @@ public class Evaluation {
 
     public boolean isEmpty = false;
     public String newResult = "this will be the result";
-    Stack<Integer> values = new Stack<Integer>();
+    Stack<Integer> operands = new Stack<Integer>();
     Stack<Character> operators = new Stack<Character>();
-       
-    
-     public String evaluate(String fromInput) {
-            char[] tokens = fromInput.toCharArray();
-            
-            for(int i=0; i <tokens.length; i++){        
-                //skip white spaces
-                if (tokens[i] == ' ')
+
+    public String evaluate(String fromInput) {
+        char[] token = fromInput.toCharArray();
+
+        for (int i = 0; i < token.length; i++) {
+            //skip white spaces
+            if (token[i] == ' ') {
                 continue;
+            } else if (token[i] >= '0' && token[i] <= '9') {
+                //cast token[i] to string buffer to allow Int parsing to put into operands stack
+                StringBuffer operandsBuff = new StringBuffer();
                 
-                System.out.println(tokens[i]);
-     }
-            
-               return newResult;
-     }
+            } 
+            else if (token[i] == '(') {
+                operators.push(token[i]);
+
+            } 
+            else if (token[i] == ')') {
+                operators.push(token[i]);
+            } 
+            else if ((token[i] == '+') || (token[i] == '-') || (token[i] == '*') || (token[i] == '/')) {
+                operators.push(token[i]);
+            }
+
+            return newResult;
+        }
                 
             
              
     
      
-     public static boolean isOperator(char c){
-         return( (c=='+') || (c=='/') ||(c=='*')||(c=='/'));   
-     }
+     
+
+    
+
+    public static boolean isOperator(char c) {
+        return ((c == '+') || (c == '/') || (c == '*') || (c == '/'));
+    }
 }
