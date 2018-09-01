271977586d5af660790a8ffb226b15c1e0ee32d8
luke <luke@gmail.com>
2018-09-01 06:42:37 -0400

    fonts added

--- src/infix/Infix.java
+++ src/infix/Infix.java
@@ -8,84 +8,73 @@ import javax.swing.*;
 public class Infix extends JFrame {
     //create input, button, and output components
 
-    private JLabel inputLabel = new JLabel("input", JLabel.CENTER);
-    private JLabel resultLabel = new JLabel("result", JLabel.CENTER);
-    public JTextField inputText = new JTextField("");
-    private JTextField resultText = new JTextField("");
+    public JTextField inputText = new JTextField(20);
+    private JTextField resultText = new JTextField(20);
 
     private JButton evaluateButton = new JButton("Evaluate");
 
-    public String input = "";
-    public String result = "test";
-
     //create GUI constructor
     public Infix() {
+        //create JFrame
+        JFrame frame = new JFrame("Infix Expression Evaluator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //set Font
+        Font f = new Font("Roboto", Font.PLAIN, 30);
+        //create panels
         //create inputPanel
         JPanel inputPanel = new JPanel();
-        inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.X_AXIS));
-        inputPanel.setVisible(true);
-        // inputPanel.setPreferredSize(new Dimension(600, 800));
-
-        inputLabel = new JLabel("input");
-        inputPanel.add(inputLabel);
-
-        inputText = new JTextField("");
-        inputText.setEditable(true);
-        inputPanel.add(inputText);
-
         //create JButton panel
         JPanel buttonPanel = new JPanel();
+        //create resultPanel
+        JPanel resultPanel = new JPanel();
+
+        //create Labels
+        JLabel inputLabel = new JLabel("input:  ", SwingConstants.CENTER);
+        JLabel resultLabel = new JLabel("result:  ", SwingConstants.CENTER);
+        resultText.setEditable(false);
+        //create button
         evaluateButton = new JButton("evaluate");
         evaluateButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                
+
                 Evaluation eval = new Evaluation();
-                
-                String newResult = eval.evaluate(inputText.getText());
-                
-               // String getValue = inputText.getText();
-               resultText.setText(newResult);
-                resultText.setVisible(true);
+
+                String newResult = eval.calculateExpression(inputText.getText());
+
+                String getValue = inputText.getText();
+                resultText.setText(newResult);
             }
         }
         );
+        //Set Font to fields
+        inputLabel.setFont(f);
+        inputLabel.repaint();
+        resultLabel.setFont(f);
+        resultLabel.repaint();
+        inputText.setFont(f);
+        inputText.repaint();
+        resultText.setFont(f);
+        resultText.repaint();
+        evaluateButton.setFont(f);
+        evaluateButton.repaint();
+        evaluateButton.setPreferredSize(new Dimension(500, 50));
+        
+        //add labels to Panels
+        inputPanel.add(inputLabel);
+        inputPanel.add(inputText);
         buttonPanel.add(evaluateButton);
-
-        //create resultPanel
-        JPanel resultPanel = new JPanel();
-        resultPanel.setLayout(new BoxLayout(resultPanel, BoxLayout.X_AXIS));
-        resultLabel = new JLabel("result");
         resultPanel.add(resultLabel);
-        resultText.setEditable(false);
         resultPanel.add(resultText);
-
-        //create main panel
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new GridLayout(3, 1, 5, 5));
-        mainPanel.setVisible(true);
-
-        //add thePanel to mainPanel
-        mainPanel.add(inputPanel);
-        mainPanel.add(buttonPanel);
-        mainPanel.add(resultPanel);
-        add(mainPanel, BorderLayout.CENTER);
-
-        /*    JFrame theFrame = new JFrame();
-        theFrame.add(inputPanel);
-        theFrame.add(buttonPanel);
-        theFrame.add(resultPanel);
-        theFrame.setVisible(true);
-       theFrame.add(mainPanel);
-        theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); */
+        //add Panels to frame with BorderLayout
+        frame.add(inputPanel, BorderLayout.PAGE_START);
+        frame.add(buttonPanel, BorderLayout.CENTER);
+        frame.add(resultPanel, BorderLayout.PAGE_END);
+        frame.setSize(900, 275);
+        frame.setVisible(true);
     }
 
     public static void main(String[] args) {
         Infix infix = new Infix();
-        infix.setTitle("Infix Expression Evaluator");
-        infix.setSize(2000, 600);
-        infix.setVisible(true);
-        infix.setLocationRelativeTo(null);
-        infix.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
 }
